3f58a5ee9f9bb7b7676980498cb5547037c78ded
Roy <Sonnenstein@users.noreply.github.com>
2017-01-03 14:09:44 +0100

    Included measurement counter

--- MeasureApp/js/script.js
+++ MeasureApp/js/script.js
@@ -1,18 +1,28 @@
 var clock = 0;
 var task_start = performance.now();
+var tick = 0;
+var lastTime = 0;
 
 window.ondevicemotion = function(event) { 
 	var ax = event.accelerationIncludingGravity.x;
 	var ay = event.accelerationIncludingGravity.y;
 	var az = event.accelerationIncludingGravity.z;
 
+	
 	clock = performance.now() - task_start;
+	
+	tick = tick + 1;
+	
 	
 	document.querySelector("#x_acc").innerHTML = "X = " + ax;
 	document.querySelector("#y_acc").innerHTML = "Y = " + ay;
 	document.querySelector("#z_acc").innerHTML = "Z = " + az;
 	document.querySelector("#clock_acc").innerHTML = "Clock = " + clock;
-
+	if (clock - lastTime >= 1.0) {
+		lastTime = Math.floor(clock);
+		document.querySelector("#tick_acc").innerHTML = "Ticks per Second = " + tick;
+		tick = 0;
+	}
 }
 
 window.addEventListener("deviceorientation", function(event) {
