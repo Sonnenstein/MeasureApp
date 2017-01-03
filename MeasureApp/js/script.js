32ddedb5e634c767d073ef995f42952f1aebd3d9
Roy <Sonnenstein@users.noreply.github.com>
2017-01-03 12:43:54 +0100

    Restructured project

---
+++ MeasureApp/js/script.js
@@ -0,0 +1,17 @@
+
+window.ondevicemotion = function(event) { 
+	var ax = event.accelerationIncludingGravity.x
+	var ay = event.accelerationIncludingGravity.y
+	var az = event.accelerationIncludingGravity.z
+
+	document.querySelector("#x_acc").innerHTML = "X = " + ax;
+	document.querySelector("#y_acc").innerHTML = "Y = " + ay;
+	document.querySelector("#z_acc").innerHTML = "Z = " + az;
+}
+
+window.addEventListener("deviceorientation", function(event) {
+	document.querySelector("#mag_alpha").innerHTML = "alpha = " + event.alpha;
+	document.querySelector("#mag_beta").innerHTML = "beta = " + event.beta;
+	document.querySelector("#mag_gamma").innerHTML = "gamma = " + event.gamma;
+}, true);
+
