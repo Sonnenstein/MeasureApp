d621325e9f25eb265f84c7825771cdf365028c25
Sonnenstein <robin.s@online.de>
2017-01-15 18:17:06 +0100

    Test

--- HeightApp/js/ui.js
+++ HeightApp/js/ui.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
 
-
-	
 	var measurement = [12,13,145,15];
 	for (var i = 0; i < measurement.length; i++) {
 		$("#measurementlist").append("<li><p>" + measurement[i] + " m" + "</p></li>");
