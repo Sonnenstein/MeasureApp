3e330f65f22732d76c92f4c20201be3a1212834e
Sonnenstein <robin.s@online.de>
2017-01-15 18:39:55 +0100

    Fix

--- HeightApp/js/ui.js
+++ HeightApp/js/ui.js
@@ -1,6 +1,6 @@
 function ScaleContentToDevice(){
     scroll(0, 0);
-    var content = $.mobile.getScreenHeight() - $(".ui-header").outerHeight() - $(".ui-footer").outerHeight() - $(".ui-content").outerHeight() + $(".ui-content").height();
+    var content = $.mobile.getScreenHeight() - $(".ui-header").outerHeight() - $(".ui-content").outerHeight() + $(".ui-content").height();
     $(".ui-content").height(content);
 }
 
