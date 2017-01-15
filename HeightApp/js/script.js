efc53e8ed6396ccda09b016347d2dbbf13bc98c5
Sonnenstein <robin.s@online.de>
2017-01-15 23:57:21 +0100

    Insertions

--- HeightApp/js/script.js
+++ HeightApp/js/script.js
@@ -6,8 +6,6 @@ var tick_acc = 0;
 var tick_mag = 0;
 var lastTime = task_start;
 
-
-
 // for measurement routine
 var measurementActive = false;
 var data = [];
@@ -16,6 +14,11 @@ var data = [];
 var alpha = 0.0;
 var beta = 0.0;
 var gamma = 0.0;
+
+// system calibration
+var correctionX = 0.0;
+var correctionY = 0.0;
+var correctionZ = 0.0;
 
 // measurement routine
 // -------------------------------------------------------------------------
@@ -35,6 +38,12 @@ window.ondevicemotion = function(event) {
 		newItem["beta"] = beta;
 		newItem["gamma"] = gamma;
 		data.push(newItem);
+		
+		// enough data for calibration
+		if (state == CALIBRATE && data.length >= 100) {
+			stopMeasurement();
+			performCalibration();
+		}
 	}
 
 	var outAx = (Math.round(ax * 10000) / 10000.0);
@@ -74,4 +83,78 @@ window.addEventListener("deviceorientation", function(event) {
 	document.querySelector("#mag_beta").innerHTML = "Beta: " + outBeta + "°";
 	document.querySelector("#mag_gamma").innerHTML = "Gamma: " + outGamma + "°";	
 }, true);
+
+
+// Measurement control
 // -------------------------------------------------------------------------
+// starts measurement and clears data
+function startNewMeasurement() {
+	measurementActive = false;
+	data.length = 0;
+	measurementActive = true;
+}
+
+// stops measurement and clears data
+function resetMeasurement() {
+	measurementActive = false;
+	data.length = 0;
+}
+
+// stop measurements
+function stopMeasurement() {
+	measurementActive = false;
+}
+
+// Calibration and data preparation
+// -------------------------------------------------------------------------
+// performs calibration based on measured data
+function performCalibration() {
+	prepareData();
+	var sum_x = 0.0;
+	var sum_y = 0.0;
+	var sum_z = 0.0;
+
+	for (var i = 0; i < data.length; i++) {
+		sum_x += data[i]["ax"];
+		sum_y += data[i]["ay"];
+		sum_z += data[i]["az"];
+	}
+		
+	correctionX = sum_x / data.length;
+	correctionY = sum_y / data.length;
+	correctionZ = sum_z / data.length;
+	
+	var outAx = (Math.round(correctionX * 10000) / 10000.0);
+	var outAy = (Math.round(correctionY * 10000) / 10000.0);
+	var outAz = (Math.round(correctionZ * 10000) / 10000.0);
+	
+	document.querySelector("#x_cal").innerHTML = "X: " + outAx + " m/s";
+	document.querySelector("#y_cal").innerHTML = "Y: " + outAy + " m/s";
+	document.querySelector("#z_cal").innerHTML = "Z: " + outAz + " m/s";
+	
+	ready();
+}
+
+// Transforms measurements into world space and corrects them according to calibration
+function prepareData() {
+	for (var i = 0; i < data.length; i++) {
+		
+		// transform into world space
+		var vec = [];
+		vec["x"] = data[i]["ax"];
+		vec["y"] = data[i]["ay"];
+		vec["z"] = data[i]["az"]
+		vec = transformDeviceToWorld(vec, data[i]["alpha"], data[i]["beta"], data[i]["gamma"]);
+
+		// calibrate
+		if (state != CALIBRATE) {
+			data[i]["ax"] = vec["x"] - correctionX;
+			data[i]["ay"] = vec["y"] - correctionY;
+			data[i]["az"] = vec["z"] - correctionZ;
+		} else { // state == CALIBRATE
+			data[i]["ax"] = vec["x"];
+			data[i]["ay"] = vec["y"];
+			data[i]["az"] = vec["z"];
+		}
+	}
+}
--- HeightApp/js/ui.js
+++ HeightApp/js/ui.js
@@ -1,7 +1,66 @@
 
+// current system state
+var state = -1;
+const INIT = 0;
+const CALIBRATE = 1;
+const READY = 2;
+const MEASURE = 3;
+const CALCULATE = 4;
+
 
 $(document).ready(function(){
+	calibrateScreen();
+	init();
+}); 
 
+function calibrateScreen() {
+	var screen = $.mobile.getScreenHeight();
+	var header = $(".ui-header").hasClass("ui-header-fixed") ? $(".ui-header").outerHeight()  - 1 : $(".ui-header").outerHeight();
+	var content = screen - header;
+	$(".ui-content").height(content);
+}
+
+function init() {
+	state = INIT;
+	document.querySelector("#desc").innerHTML  = "Please hold still and press button to calibrate your device.";
+	document.querySelector("#actionBtn").innerHTML = "Calibrate Device";
+}
+
+function calibrate() {
+	state = CALIBRATE;
+	document.querySelector("#desc").innerHTML  = "Please hold still and wait while your device is calibrating.";	
+	document.querySelector("#actionBtn").innerHTML = "Now Calibrating";
+	startNewMeasurement();
+}
+
+function ready() {
+	alert("X: " + correctionX + " Y: " + correctionY + " Z: " + correctionZ);
+	state = READY;
+	document.getElementById("actionBtn").value = "Start Measurement";
+}
+
+
+function performAction() {
+	switch (state) {
+		case INIT: calibrate();
+			break;
+		case CALIBRATE: document.getElementById("actionBtn").value = "Please wait";
+			break;
+		case READY: measure();
+			break;
+		case MEASURE: calculate();
+			break;
+		case CALCULATE: document.getElementById("actionBtn").value = "Please wait";
+			break;
+		default: alert("Invalid state: " + state);
+			break;
+	}
+}
+
+
+
+
+/*
 	var measurement = [12,13,145,15];
 	for (var i = 0; i < measurement.length; i++) {
 		$("#measurementlist").append("<li><p>" + measurement[i] + " m" + "</p></li>");
@@ -13,10 +72,5 @@ $(document).ready(function(){
 	}
 	// vmousedown
 	// vmouseup
-	
-	var screen = $.mobile.getScreenHeight();
-	var header = $(".ui-header").hasClass("ui-header-fixed") ? $(".ui-header").outerHeight()  - 1 : $(".ui-header").outerHeight();
-	var content = screen - header;
-	$(".ui-content").height(content);
-	
-}); 
+
+*/
