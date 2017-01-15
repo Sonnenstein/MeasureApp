80c7892531954503daa31665d6ebb5d388f59173
Sonnenstein <robin.s@online.de>
2017-01-15 19:39:07 +0100

    Test

--- HeightApp/js/ui.js
+++ HeightApp/js/ui.js
@@ -16,8 +16,7 @@ $(document).ready(function(){
 	
 	var screen = $.mobile.getScreenHeight();
 	var header = $(".ui-header").hasClass("ui-header-fixed") ? $(".ui-header").outerHeight()  - 1 : $(".ui-header").outerHeight();
-	var contentCurrent = $(".ui-content").outerHeight() - $(".ui-content").height();
-	var content = screen - header - contentCurrent;
+	var content = screen - header;
 
 	$(".ui-content").height(content);
 }); 
