41c045b2809cb510f7631a31671c65294e680f6a
Sonnenstein <robin.s@online.de>
2017-01-15 11:39:22 +0100

    Fix

---
+++ HeightApp/js/ui.js
@@ -0,0 +1,16 @@
+/*
+var countries = ['United States', 'Canada', 'Argentina', 'Armenia'];
+var cList = $('ul.mylist')
+
+$.each(countries, function(i)
+{
+    var li = $('<li/>')
+        .addClass('ui-menu-item')
+        .attr('role', 'menuitem')
+        .appendTo(cList);
+    var aaa = $('<a/>')
+        .addClass('ui-all')
+        .text(countries[i])
+        .appendTo(li);
+});
+*/
